c75c8d5afe27377ee7599b3870cfb2435e13b464
thanhtungetm <thanhtungetm@gmail.com>
2021-10-16 19:38:04 +0700

    Tao trang them mot  lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h4>Thêm mới liên hệ</h4>
+            <ContactForm 
+                :contact='{}'
+                @contact-submit='addContact'
+            />
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from '../components/ContactForm.vue'
+import ContactService from '../services/contact.service'
+
+export default {
+    name: 'ContactAdd',
+    components: {
+        ContactForm,
+    },
+    data(){
+        return {
+            message: '',
+        }
+    },
+    methods: {
+        async addContact(data){
+            const [error, response] = await this.handle(ContactService.create(data))
+
+            if(error){
+                console.log(error)
+            }else{
+                console.log(response.data)
+                this.message = 'Đã thêm thành công'
+            }
+            console.log(data)
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
